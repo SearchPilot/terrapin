54708b0536254689b54cc001d293294f1d18a7e9
Austin Platt <austinplatt@gmail.com>
2016-06-02 16:23:23 +0100

    rearrange project for pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='terrapin',
-      version="0.0.4",
+      version="0.0.5",
       packages=find_packages(),
       description='A very lightweight template language',
       url='https://github.com/DistilledLtd/terrapin',
