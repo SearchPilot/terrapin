23ffdbb76647440964429da6b8a3bdf4b67e271f
Duncan Morris <duncan.morris@distilled.net>
2016-06-03 14:27:35 +0100

    Raise templateerror for lex errors aswell

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='terrapin',
-      version="0.0.8",
+      version="0.0.9",
       packages=find_packages(),
       description='A very lightweight template language',
       url='https://github.com/DistilledLtd/terrapin',
