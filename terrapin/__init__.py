4916132dfdf7fcc69d41260e3bee109c8afe4a06
Austin Platt <austinplatt@gmail.com>
2016-06-07 13:23:28 +0100

    use fallback render for simple templates

--- terrapin/__init__.py
+++ terrapin/__init__.py
@@ -1,6 +1,48 @@
+import re
+
 from terrapin.parser import Parser
 
 
-def render(template, context):
+parser = Parser()
 
-    return Parser().parse(template, context)
+
+def render(template, context):
+    """ Choose which rendering strategy to use and execute """
+
+    if '{%' in template:
+        return render_full(template, context)
+    else:
+        return render_simple(template, context)
+
+
+def render_full(template, context):
+    """ Render with fully featured template engine """
+
+    return parser.parse(template, context)
+
+
+def render_simple(template, context):
+    """ Render a template by replacing variables """
+
+    return replace_variables(template, context)
+
+
+def replace_variables(string, variables):
+    """ Format a templated value """
+
+    found_variables = find_variables(string)
+    for variable in found_variables:
+        value = str(variables.get(variable, ''))
+        string = string.replace('{{' + variable + '}}', value)
+    return string
+
+
+def find_variables(template):
+    """ Find all the used variables
+
+    This is any string that looks like {{var}}
+    """
+
+    capture_variable_regex = '\{\{([\w\.]+?)\}\}'
+    found_variables = re.findall(capture_variable_regex, template)
+    return list(found_variables)
