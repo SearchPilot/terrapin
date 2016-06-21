39b0146c95a5585399dabed884e11a40fafe41f3
austin-distilled <austin.platt@distilled.co.uk>
2016-06-21 10:20:09 +0100

    introduce function to see what variables are used (#17)
    * introduce function to see what variables are used
    
    * increment version number
    
    * tidy up file structure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='terrapin',
-      version="0.0.14",
+      version="0.0.15",
       packages=find_packages(),
       description='A very lightweight template language',
       url='https://github.com/DistilledLtd/terrapin',
--- terrapin/__init__.py
+++ terrapin/__init__.py
@@ -1,10 +1,6 @@
-import re
-
-from terrapin.parser import Parser
-from terrapin.lexer import word_regex
-
-
-parser = Parser()
+from terrapin.render_full import render_full
+from terrapin.render_simple import render_simple
+from terrapin.used_variables import used_variables
 
 
 def render(template, context):
@@ -14,36 +10,3 @@ def render(template, context):
         return render_full(template, context)
     else:
         return render_simple(template, context)
-
-
-def render_full(template, context):
-    """ Render with fully featured template engine """
-
-    return parser.parse(template, context)
-
-
-def render_simple(template, context):
-    """ Render a template by replacing variables """
-
-    return replace_variables(template, context)
-
-
-def replace_variables(string, variables):
-    """ Format a templated value """
-
-    found_variables = find_variables(string)
-    for variable in found_variables:
-        value = str(variables.get(variable, ''))
-        string = string.replace('{{' + variable + '}}', value)
-    return string
-
-
-def find_variables(template):
-    """ Find all the used variables
-
-    This is any string that looks like {{var}}
-    """
-
-    capture_variable_regex = r'\{\{(' + word_regex + r')\}\}'
-    found_variables = re.findall(capture_variable_regex, template)
-    return list(found_variables)
